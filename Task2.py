4d36763a3602f7d5158658da8d55a9a66a1faddf
harkirat253 <s26604@caseygrammar.vic.edu.au>
2025-09-14 23:08:20 +0000

    task 2 complete

--- Task2.py
+++ Task2.py
@@ -1,10 +1,6 @@
-# Task 2: James Bond
-def main():
-    #Write your code for input here
-    
+def james_bond_greeting():
+    first_name = input("What is your first name? ")
+    last_name = input("What is your last name? ")
+    print(f"My name is {last_name}, {first_name} {last_name}")
 
-
-    # End of your code for input here
-
-if __name__ == '__main__':
-    main()
+james_bond_greeting()
