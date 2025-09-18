28d2f24b9487e5c955cb1d636a6294d270210a87
harkirat253 <s26604@caseygrammar.vic.edu.au>
2025-09-18 02:21:21 +0000

    task 1 complete

--- task1.py
+++ task1.py
@@ -2,9 +2,10 @@
 # Task 1  Input: Hello
 def main():
     # Write your code for input here
-    
-    var = input('What is your name: ')
-    print(f'Hi, {var}')
+   
+    name = input("What is your name? ")
+    print(f"Hello, {name}")
+
 
     # End of your code for input here
 
