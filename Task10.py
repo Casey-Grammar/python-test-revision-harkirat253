7e6c8cefa663b999199fde07ced364716802de06
harkirat253 <s26604@caseygrammar.vic.edu.au>
2025-09-18 01:50:53 +0000

    task 10 complete

--- Task10.py
+++ Task10.py
@@ -1,24 +1,11 @@
-# Task 10 Class roll
-# Write a program that takes a list of student names and sorts them
-# to create a class roll. The list of names will be given on a one line
-# separated by a single space.
+# Get the student names as input
+student_names = input("Enter the student names separated by spaces: ").split()
 
-#The student names could be entered with any combination of capitals or not.
-#If they are entered with no capital first letter the list should not append.
-#If they are entered with a capital first letter the list should append
-#If they are entered with a mix of capitals the name should be converted to capital first letter and
-#then added to the list.
+# Sort the names and create the class roll
+class_roll = sorted([name.capitalize() for name in student_names])
 
-def main():
-    #Write your code here
-    
-
-
-    # End of your code here
-
-
-
-
-
-if __name__ == '__main__':
-    main()
+# Print the class roll
+print("Students:", " ".join(student_names))
+print("Class Roll:")
+for name in class_roll:
+    print(name)
