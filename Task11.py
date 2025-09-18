7967ebd45ff77811226cbef0bdfd4eb69bd8ed3d
harkirat253 <s26604@caseygrammar.vic.edu.au>
2025-09-18 01:53:28 +0000

    task 11 complete

--- Task11.py
+++ Task11.py
@@ -1,24 +1,10 @@
-# Task 11 Fix the errors in the code
-# Fix the errors in this code by running the program with different inputs
-# and editing the code until all errors are fixed.
-
-def main():
-    #Fix the code starting here
-    drink = input("WHAT DRINK DO YOU WANT TO ORDER?)
-    if drink.islower():
-     print("I DIDN'T HEAR YOUR ORDER!"
-      elif drink.isupper():
-      many = input("HOW MANY? ")
-    coffee scoops = many * 4
+drink = input("WHAT DRINK DO YOU WANT TO ORDER? ")
+if drink.islower():
+    print("I DIDN'T HEAR YOUR ORDER!")
+elif drink.isupper():
+    many = int(input("HOW MANY? "))
+    coffee_scoops = many * 4
     print(f'{many} {drink}S COMING RIGHT UP!')
-    print(f'The barrister needs {coffee scoops} scoops of coffee in the coffee making machine')
-    
-
-# End of code to be fixed
-
-
-
-
-
-if __name__ == '__main__':
-    main()
+    print(f'The barista needs {coffee_scoops} scoops of coffee in the coffee making machine')
+else:
+    print("Please enter the drink in all uppercase or all lowercase.")
